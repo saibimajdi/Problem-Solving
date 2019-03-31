f6038bddf90711e1dc1df54527f1ffa4a8d6cd8d
saibimajdi <saibimajdi@outlook.com>
2019-03-31 14:27:35 -0600

    added dp

--- Longest Common Subsequence/LCS.py
+++ Longest Common Subsequence/LCS.py
@@ -1,19 +1,48 @@
-def LCS(A, B, j, k):
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+
+def LCS(A, B, j, k, dp):
+
+
     if j == len(A) or k == len(B):
         return ""
 
-    if A[j] == B[k]:
-        return A[j] + LCS(A,B, j+1, k+1)
 
-    resA = LCS(A,B, j+1, k)
-    resB = LCS(A,B, j, k + 1)
+    if dp[j][k] != None:
+        return dp[j][k]
+
+    if A[j] == B[k]:
+        dp[j][k] = A[j] + LCS(A,B, j+1, k+1, dp)
+        return dp[j][k]
+
+    resA = LCS(A,B, j, k + 1, dp)
+    resB = LCS(A,B, j + 1, k, dp)
 
     if len(resA) > len(resB):
         return resA
     else:
         return resB
 
-A = "ABCAS33AS"
-B = "BASWQSSAASD"
-print(LCS(A,B,0,0))
 
+
+_input = input().split(' ')
+n = int(_input[0])
+m = int(_input[1])
+
+dp = []
+
+_A = input()
+_B = input()
+
+A = _A.replace(" ", "")
+B = _B.replace(" ", "")
+
+dp = [[None for i in range(m)] for i in range(n)]
+
+res = LCS(A,B, 0, 0, dp)
+
+ans = ""
+for x in res:
+    ans = ans + x + " "
+
+print(ans)
