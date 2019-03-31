4858198e0c154fb1a1d7febb256e198be4a0de90
saibimajdi <saibimajdi@outlook.com>
2019-03-30 22:21:42 -0600

    LCS

---
+++ Longest Common Subsequence/LCS.py
@@ -0,0 +1,22 @@
+def LCS(A, B, j, k):
+    if j == len(A) or k == len(B):
+        return ""
+
+    if A[j] == B[k]:
+        return A[j] + LCS(A,B, j+1, k+1)
+
+    resA = LCS(A,B, j+1, k)
+    resB = LCS(A,B, j, k + 1)
+
+    if len(resA) > len(resB):
+        return resA
+    else:
+        return resB
+
+A = "ABCAS33AS"
+B = "BASWQSSAASD"
+
+res = LCS(A,B,0,0)
+
+print(res)
+
